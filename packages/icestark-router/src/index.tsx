56eea9d24403221554ed3314c065b9fba6acb636
daysai <470040845@qq.com>
2019-07-03 16:41:53 +0800

    feat: support children

--- packages/icestark-router/src/index.tsx
+++ packages/icestark-router/src/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { getBasename } from '@ice/stark';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-interface MyRoute {
+interface MyChild {
   component: any;
   path: string | string[];
+  redirect?: string;
   key?: string;
   name?: string;
   exact?: boolean;
@@ -12,18 +13,47 @@ interface MyRoute {
   sensitive?: boolean;
 }
 
+interface MyRoute extends MyChild {
+  children?: MyChild[];
+}
+
 export interface StarkRouterProps {
   routes: MyRoute[];
 }
+
+const RouteItem: React.SFC<MyChild> = (props: MyChild) => {
+  const { redirect, path, key, ...others } = props;
+  if (redirect) {
+    return <Redirect exact key={key} from={path as string} to={redirect} />;
+  }
+  return <Route key={key} path={path} {...others} />;
+};
 
 const StarkRouter: React.SFC<StarkRouterProps> = (props: StarkRouterProps) => {
   const { routes } = props;
   return (
     <Router basename={getBasename()}>
       <Switch>
-        {routes.map(route => {
-          const { key, name, component, ...others } = route;
-          return <Route key={key || name} {...others} component={component} />;
+        {routes.map((route, id) => {
+          const { children, component: RouteComponent, key, name, path, ...others } = route;
+          return children ? (
+            <Route
+              {...others}
+              key={key || name || `${id}`}
+              render={props => (
+                <RouteComponent {...others} {...props}>
+                  <Switch>
+                    {children.map((child, cId) => {
+                      const { key: cKey, name: cName } = child;
+                      return <RouteItem {...child} key={cKey || cName || `${id}-${cId}`} />;
+                    })}
+                  </Switch>
+                </RouteComponent>
+              )}
+            />
+          ) : (
+            <RouteItem {...route} key={key || name || `${id}`} />
+          );
         })}
       </Switch>
     </Router>
