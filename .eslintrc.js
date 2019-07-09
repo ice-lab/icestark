6af21240dcf7616df092178050bd4c6bd66a6e11
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-07-09 18:08:45 +0800

    chore: use @ice/spce

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+const { eslintTS, deepmerge } = require('@ice/spec');
+
+module.exports = deepmerge(eslintTS, {
+  env: {
+    jest: true
+  },
+  rules: {
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/interface-name-prefix": 1,
+  },
+  settings: {
+    "react": {
+      "pragma": "React",
+      "version": "detect"
+    }
+  },
+});
