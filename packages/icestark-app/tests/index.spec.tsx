51084beef831cad230c00d2681f30f7843aa8dfa
daysai <wddpwzz36@gmail.com>
2019-11-13 11:54:36 +0800

    release: icestark@1.1.0 (#27)
    * fix: rm localUrl api
    
    * fix: rm localUrl
    
    * chore: migrate testing-library
    
    ref https://github.com/testing-library/dom-testing-library/issues/260
    
    * feat: add AppRoute render/component props
    
    * chore: rm setupFilesAfterEnv
    
    * fix: adjust for cr
    
    * fix: adjust JS asset to js asset
    
    * fix: rm useless cleanup-after-each
    
    * feat: add AppRouteComponentProps ts
    
    * chore: update version
    
    * fix: interface match to Match

--- packages/icestark-app/tests/index.spec.tsx
+++ packages/icestark-app/tests/index.spec.tsx
@@ -1,4 +1,4 @@
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 
 import {
   getBasename,
--- src/appHistory.ts
+++ src/appHistory.ts
@@ -1,20 +1,25 @@
-const appHistory = {
-  push: (url: string) => {
+export interface AppHistory {
+  push(path: string): void;
+  replace(path: string): void;
+}
+
+const appHistory: AppHistory = {
+  push: (path: string) => {
     window.history.pushState(
       {
         forceRender: true,
       },
       null,
-      url,
+      path,
     );
   },
-  replace: (url: string) => {
+  replace: (path: string) => {
     window.history.replaceState(
       {
         forceRender: true,
       },
       null,
-      url,
+      path,
     );
   },
 };
