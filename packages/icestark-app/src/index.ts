fd375114ecf36d1fa8341e8fbf50602abb594ab4
daysai <wddpwzz36@gmail.com>
2019-12-10 16:14:55 +0800

    feat: add registerAppEnter

--- packages/icestark-app/src/index.ts
+++ packages/icestark-app/src/index.ts
@@ -1,5 +1,6 @@
 export { default as getMountNode } from './getMountNode';
 export { default as renderNotFound } from './renderNotFound';
 export { default as getBasename } from './getBasename';
+export { default as registerAppEnter } from './registerAppEnter';
 export { default as registerAppLeave } from './registerAppLeave';
 export { default as appHistory } from './appHistory';
