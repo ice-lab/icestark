773f9d966792199f0ca22dc9455e3070512d5dfd
daysai <wddpwzz36@gmail.com>
2019-12-23 19:10:19 +0800

    feat: adjust callAppEnter 2 promise function

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -247,7 +247,7 @@ export default class AppRoute extends React.Component<AppRouteProps, AppRouteSta
       if (this.unmounted || this.prevAppConfig !== prevAppConfig) return;
 
       // trigger sub-application render
-      callAppEnter();
+      await callAppEnter();
 
       // cancel loading after handleAssets
       handleLoading(false);
--- src/util/appLifeCycle.ts
+++ src/util/appLifeCycle.ts
@@ -1,16 +1,16 @@
 import { setCache, getCache } from './cache';
 import { resetPopStateListeners } from './capturedListeners';
 
-export function callAppEnter() {
+export async function callAppEnter() {
   const registerAppEnterCallback = getCache('appEnter');
 
   if (registerAppEnterCallback) {
-    registerAppEnterCallback();
+    await registerAppEnterCallback();
     setCache('appLeave', null);
   }
 }
 
-export function callAppLeave() {
+export async function callAppLeave() {
   // resetPopStateListeners when app change, remove react-router/vue-router listeners
   resetPopStateListeners();
 
