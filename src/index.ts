0c408cbe44cddf3e69ca031650349a3360a5f698
ClarkXia <xiawenwu41@gmail.com>
2020-09-22 17:28:37 +0800

    fix: export apis

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export { default as AppRouter } from './AppRouter';
 export { default as AppRoute } from './AppRoute';
 export { default as AppLink } from './AppLink';
 export { default as appHistory } from './appHistory';
-export { loadMicroApp, registerMicroApps } from './apps';
+export { createMicroApp, registerMicroApps } from './apps';
 export { default as start } from './start';
