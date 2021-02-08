6caaf648bcfe90b299ddc0150960ea1990753031
maoxiaoke <maoxiaoke@outlook.com>
2021-02-08 17:11:21 +0800

    feat: 🎸 add setBasename fun

--- packages/icestark-app/src/index.ts
+++ packages/icestark-app/src/index.ts
@@ -1,6 +1,7 @@
 export { default as getMountNode } from './getMountNode';
 export { default as renderNotFound } from './renderNotFound';
 export { default as getBasename } from './getBasename';
+export { default as setBasename } from './setBasename';
 export { default as registerAppEnter } from './registerAppEnter';
 export { default as registerAppLeave } from './registerAppLeave';
 export { default as appHistory } from './appHistory';
