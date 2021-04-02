c4744e18c0284cbbbc686f1c2c53098270d02141
maoxiaoke <maoxiaoke@outlook.com>
2021-04-02 17:39:15 +0800

    ci: 🎡 cp files to docs

---
+++ scripts/docsBuild.ts
@@ -0,0 +1,21 @@
+/**
+ * scripts to build docs
+ */
+import { spawnSync } from 'child_process';
+import { copySync } from 'fs-extra';
+import { join } from 'path';
+
+console.log('[DOCS:BUILD]', process.cwd());
+
+spawnSync('npm', [
+  'run',
+  'docs:build',
+], {
+  stdio: 'inherit',
+  cwd: process.cwd(),
+});
+
+const source = join(__dirname, '../dist');
+const target = join(__dirname, '../docs');
+
+copySync(source, target);
