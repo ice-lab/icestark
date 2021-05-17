70e4d75f25f70feeaec43cd76393544e9bad7c51
maoxiaoke <maoxiaoke@outlook.com>
2021-05-17 15:36:11 +0800

    fix: 🐛 eval should not be bind
    ✅ Closes: https://github.com/alibaba/ice/issues/4294

--- packages/icestark-sandbox/tests/index.spec.ts
+++ packages/icestark-sandbox/tests/index.spec.ts
@@ -73,3 +73,26 @@ describe('sandbox: falsy values should be trapped.', () => {
     expect((window as any).a).toBe(undefined);
   });
 });
+
+describe('eval in sandbox', () => {
+  const sandbox = new Sandbox({ multiMode: true });
+
+  test('execution context is not global execution context', () => {
+    let error = null;
+    try {
+      sandbox.execScriptInSandbox(
+        `
+          function bar (value) {
+            eval('console.log(value);');
+          }
+          bar(1);
+        `
+      );
+    } catch (e) {
+      error = e.message;
+    }
+
+    expect(error).toBe(null);
+  });
+});
+
