fc90181e9cb8ecbfd96b0a39024f4759f5d87ccd
jianleiHuang <huangmorejs1213@gmail.com>
2021-12-28 20:58:31 +0800

    bugfix: ignore error when check libray export (#503)
    * bugfix: ignore error when check libray export
    
    Co-authored-by: = <=>

--- packages/icestark-module/src/utils.ts
+++ packages/icestark-module/src/utils.ts
@@ -1,5 +1,11 @@
 export const any2AnyArray = <T>(any: T | T[]): T[] => (Array.isArray(any) ? any : [any]);
 
 export const isEsModule = (esModule) => {
-  return esModule && esModule.__esModule;
+  try {
+    return esModule && esModule.__esModule;
+  } catch (error) {
+    // 在某些场景下，例如：iframe，访问属性会存在跨域问题，会直接报错
+    // 这种情况，直接忽略不做diff比较
+    return false;
+  }
 };
