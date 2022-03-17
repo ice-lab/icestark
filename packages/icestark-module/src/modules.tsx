264ed931f1f53471dd1fc3bc9c2ff6ee806237d9
maoxiaoke <maoxiaoke@outlook.com>
2022-03-17 12:19:14 +0800

    refactor: 💡 eslint

--- packages/icestark-module/src/modules.tsx
+++ packages/icestark-module/src/modules.tsx
@@ -83,7 +83,6 @@ export const registerModules = (modules: StarkModule[]) => {
 
 const filterRemoveCSS = (cssList: string[]) => {
   return (cssList || []).filter((cssLink) => {
-    // cssStorage[cssLink] may undefined, when unMount is earlier than exe js module  
     if (cssStorage[cssLink]?.count > 1) {
       cssStorage[cssLink].count -= 1;
       return false;
