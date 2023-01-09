1d4ed878dd0ddbf6377ed75d139346be87972afc
Sarasa Ryuuou <zyp108421@gmail.com>
2023-01-09 10:46:59 +0800

    feat(module): add empty & equal checking to shallowCompare

--- packages/icestark-module/src/assist.ts
+++ packages/icestark-module/src/assist.ts
@@ -1,3 +1,5 @@
 export function shallowCompare<T extends object>(source: T, target: T) {
+  if (source === target) return true;
+  if (!source || !target) return false;
   return Object.keys(source).every((key) => source[key] === target[key]);
 }
